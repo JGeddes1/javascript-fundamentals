c28d0c9a1253374f909d10cb4c4c77db8b8ffe09
JGeddes1 <jgeddes513@gmail.com>
2020-11-10 14:34:24 +0000

    2:09:04 in

--- app.js
+++ app.js
@@ -3,6 +3,55 @@
 //console.log('hello world);
 
 
-// Variables
+// ! Variables
 
-let 
+// let name = "John";
+// let address,zip ,state;
+// console.log(address);
+// console.log(name);
+
+// LET VS CONST VS VAR
+
+// var value = "some value";
+
+// let name = "john";
+
+// const lastName = "jordan"; //cant re assign
+
+
+// !String Concatenation - combine string values
+
+// const name = "john";
+// const lastName = "shaebeake"
+// let fullName = name + ' ' + lastName;
+
+
+// console.log("hello there your full name is : " + name + " " + lastName );
+// console.log();
+
+// ! basic numbers
+// const number = 34;
+// const number2 = 2.456;
+// const add = number + number2;
+// const sub = number - number2;
+// const mult = number * number2;
+// const divide = number / number2;
+// 
+
+//! additional features
+let number = 40;
+number += 5; //add 5
+number += 5; //add 5
+number += 5; //add 5
+number += 5; //add 5
+number += 5; //add 5
+
+// ? Modulus (%) operator returns remainder aftrer integer division
+const slices = 10;
+const children = 3;
+const amount = slices % children;
+
+// ?BIDMAS basiclayy
+
+//!Implicit typer conversion
+
