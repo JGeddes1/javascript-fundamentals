29e824863825916db802efc2097c3b93b9413971
JGeddes1 <jgeddes513@gmail.com>
2020-11-19 18:34:21 +0000

    up to Mouse Events

--- app.js
+++ app.js
@@ -628,17 +628,563 @@ function hello(){
 //     console.log(`Good afternoon ${name}, my name is ${myName} `);
 // }
 
-function morning(name){
+// function morning(name){
 
-    return `good morning ${name.toUpperCase()}`;
-}
+//     return `good morning ${name.toUpperCase()}`;
+// }
 
-function greet(name, cb) {
+// function greet(name, cb) {
     
-    const myName = 'Jamie';
-    console.log(`${cb(name)}, my name is ${myName} `);
-}
+//     const myName = 'Jamie';
+//     console.log(`${cb(name)}, my name is ${myName} `);
+// }
 
 
 
-greet('bobo', morning);
+// greet('bobo', morning);
+
+// !ARRAY ITERATORS
+// Powerful array methods
+// forEach, map, find, filter, reduce
+// iterate over array - no loop required
+// accepts CALLBACK function as argument
+const numbers = [0,1,2,3,4];
+
+// show all numbers
+
+// for (let i = 0; i < numbers.length; i++) {
+//     const element = numbers[i];
+//     console.log(element);
+// }
+
+// ?forEach
+// dopes not return new array
+
+// const people = [
+//     {name:'bob', age:20, position:'developer'},
+//     {name:'peter', age:25, position:'designer'},
+//     {name:'susy', age:30, position:'THEBOSS'},
+// ];
+
+// function showPerson(person){
+// console.log(person.position.toUpperCase());
+// }
+
+
+// // people.forEach(showPerson);
+ 
+// people.forEach(function(item){
+//     console.log(item.position.toUpperCase());
+// }
+// );
+
+// ?MAP
+// returns a new array
+// does not change size of original array
+// uses values from origianl array when making new one
+
+// const people = [
+//     {name:'bob', age:20, position:'developer'},
+//     {name:'peter', age:25, position:'designer'},
+//     {name:'susy', age:30, position:'THEBOSS'},
+// ];
+
+// const ages = people.map(function(person){
+    
+//     return person.age + 20;
+// });
+// const newPeople = people.map(function(person){
+//     return {
+//         firstName: person.name.toUpperCase(),
+//         oldAge: person.age + 20,
+//     }
+// });
+
+
+// const names = people.map(function(person){
+//     return `<h1>${person.name}</h1>`;
+// })
+
+// document.body.innerHTML = names.join('');
+
+
+// console.log(names);
+
+
+// ? Filter
+// does returna  new array
+//  can minpulate the size of a new array
+// returns based on condition
+
+
+
+// const people = [
+//     {name:'bob', age:20, position:'developer'},
+//     {name:'peter', age:25, position:'designer'},
+//     {name:'susy', age:30, position:'THEBOSS'},
+// ];
+
+// const youngPeople = people.filter(function (person){
+//     return person.age <= 25;
+// });
+
+// console.log(youngPeople);
+
+
+// ?find
+
+// returns an object
+// returns first match, if no match undefined
+// great for getting unique values
+
+// const people = [
+//     {name:'bob', age:20, position:'developer', id: 1},
+//     {name:'peter', age:25, position:'designer', id: 2},
+//     {name:'susy', age:30, position:'THEBOSS', id: 3},
+// ];
+
+
+// const person = people.find(function(person){
+//     return person.id ===3;
+// });
+
+// console.log(person);
+
+
+// ? Reduce
+// iterates, callback function
+// reduces to a single value - number, aray, object
+//  1 paramter ('acc') - total of all calculation
+// 2 paramter ('curr') - current iteration/value
+
+
+// const people = [
+//     {name:'bob', age:20, position:'developer', id: 1, salary:200},
+//     {name:'peter', age:25, position:'designer', id: 2, salary:300},
+//     {name:'susy', age:30, position:'THEBOSS', id: 3, salary:500},
+//     {name:'anna', age:30, position:'THEBOSS', id: 4, salary:500},
+// ];
+
+
+// const total = people.reduce(function(acc,currItem) {
+//     console.log(`total ${acc}`);
+//     console.log(`current money ${currItem.salary}`);
+//     acc += currItem.salary;
+
+
+//     return acc;
+// }, 0);
+
+// console.log(total);
+
+// !MATH 
+// standard built in objects - always available
+// nifty global objects to use without extra setup
+
+// const number = 4.56789;
+// const result = Math.floor(number) // rounds number down
+
+// const number = 4.1222;
+// const result = Math.ceil(number) // rounds number up
+
+// const number = 25;
+// const result = Math.sqrt(number) // sqaure root
+
+// const result = Math.PI // PI
+// const result = Math.min(4,6,5,7,9) ;// min number
+// const result = Math.max(4,6,5,7,9) ;// max number
+
+// const result = Math.floor( Math.random()*10 + 1);
+
+// console.log(result);
+
+
+// ! date
+
+// const months = [
+// 'January',
+// 'Feb',
+// 'March',
+// 'April',
+// 'June',
+// 'July',
+// 'August',
+// 'Sept',
+// 'Oct',
+// 'Novemb',
+// 'December',
+// ];
+
+// const days = [
+// 'Sunday',
+// 'Monday',
+// 'Tuesday',
+// 'Wednesday',
+// 'Thursday',
+// 'Friday',
+// 'Saturday'
+// ];
+// const date = new Date('1/12/2004')
+// const date = new Date();
+// const month = date.getMonth();
+// console.log(months[month]);
+
+// const day = date.getDay();
+// console.log(days[day]);
+
+// console.log(date.getDate());
+// console.log(date.getFullYear());
+
+// const sentence = `${days[day]}, ${date.getDate()} ${months[month]} ${date.getFullYear()}`;
+
+// document.body.innerHTML = sentence;
+
+// console.log(sentence);
+
+
+
+// ! DOM
+// ? select elements
+// traverse DOM
+// insert/remove elements
+// apply styling
+// add/remove css classes
+// .....
+
+// Document object model
+
+// similar to css
+// select the element or gourp of elements the you want ot affect
+// decide the effect we want to applyt o the selection
+// Many different ways
+
+//  document.body.style.backgroundColor = 'blue';
+//  document.body.style.color = 'yellow';
+//  document.getElementById('btn').style.color = 'red';
+
+// // assign to a variable
+
+// const element = document.getElementById('element');
+// // do something
+
+// document.querySelector('element'); //Do someting
+
+// // window object
+// // console.log(window);
+
+// // returns a node object or a node list, which is an array like object
+
+// const btn = document.getElementById('btn');
+// const name = btn.nodeName;
+// const css = btn.style;
+// console.log(btn);
+// console.log(name);
+// console.log(css);
+
+
+// ! WINDOW OBJECT = browser api
+//the tab you are on is the window
+// !GetElementById
+
+// const h1 = document.getElementById('title');
+// h1.style.color = 'red';
+
+// const btn = document.getElementById('btn');
+
+// btn.style.backgroundColor = 'blue';
+// btn.style.color = 'white';
+
+
+// !GET Elements By Tag Name
+// getElementsByTagName('tagname');
+// node-list = array-like object
+// index, length property but not array methods
+
+// const headings = document.getElementsByTagName('h2');
+// headings[0].style.color = 'red';
+// // console.log(headings.length);
+
+// const items = document.getElementsByTagName('li');
+
+// // items.forEach(function(item){
+// //     console.log(item);
+// // })
+// items[2].style.color = 'orange';
+// const betterItems = [...items];
+
+// betterItems.forEach(function(item){
+// // console.log(item);
+// });
+
+// console.log(items);
+// console.log(betterItems);
+
+// ! getElementsByClassName('classname')'
+// node-list = array-like object
+// index, length property but not array methods
+
+// const listItems = document.getElementsByClassName('special');
+// // console.log(listItems);
+// listItems[1].style.color = 'blue';
+
+
+// ! querySelector('any css') - slects single element
+// !querySelectorAll('any css') - selects all
+
+// can use forEach method
+
+
+// const  result = document.querySelector('#result');
+// result.style.backgroundColor = 'blue';
+
+// const item = document.querySelector('.special');
+// // console.log(item);
+
+// const lastItem = document.querySelector('li:last-child');
+// // console.log(lastItem);
+
+// const list = document.querySelectorAll('.special');
+// console.log(list);
+
+// list.forEach(function(item){
+//     console.log(item);
+//     item.style.color = 'yellow';
+// });
+
+
+// ! Naviagte the DOM - Children
+// ?select the element or goupr of elements the we WebAuthentication
+// decide the ffect we want to apply to the section
+
+// childNodes - returns all childNodes including whitespace which is treated as a text node
+
+// children
+// firstChild
+// lastChild
+
+// const result = document.querySelector('#result');
+// const allChildren = result.childNodes;
+// // console.log(allChildren);
+
+// const childrens = result.children
+// console.log(childrens);
+// console.log(result.firstChild);
+// console.log(result.lastChild);
+
+
+// !PARENT ELEMENT
+
+// const heading = document.querySelector('h2');
+// const parent = heading.parentElement;
+// parent.style.color = 'red';
+
+
+// !previousSibling
+// nextSibling
+// return whitespace
+
+
+//!nodeValue
+// !textContent
+
+// const item = document.getElementById('special');
+// const value = item.firstChild.nodeValue;
+
+
+// const easyValue = item.textContent
+// console.log(easyValue);
+
+
+
+// !~getArttibute();
+// !setAttribute();
+
+
+// const first = document.querySelector('.first');
+// const idValue = first.getAttribute('id');
+// // console.log(idValue);
+
+
+// const link = document.getElementById('link');
+// const showLink = link.getAttribute('href');
+// console.log(showLink);
+
+// const links = document.querySelectorAll('.first');
+// console.log(links);
+
+// const last = link.nextElementSibling;
+// last.setAttribute('class', 'first');
+// last.textContent = 'i also have a class of first';
+// console.log(last);
+
+
+//! className
+//! classList
+
+// const first = document.getElementById('first');
+// const second = document.getElementById('second');
+// const third = document.getElementById('third');
+
+// // const classValue = first.className;
+
+// // console.log(classValue);
+
+
+// second.className = 'colors';
+
+
+
+
+// // third.classList.add('colors');
+// // third.classList.add('text');
+// third.classList.add('text', 'colors');
+// third.classList.remove('text');
+
+// let result = third.classList.contains('colors');
+// if (result) {
+//     console.log('hello world');
+// }else{
+//     console.log('doesnt have class');
+// }
+
+// const classValue = third.classList;
+// console.log(classValue);
+
+//! CreateElement
+// createElement('element')
+// createTextNode('text content')
+// ALWAYS DO THE TWO ABOVE THEN CHOSE FROM BLUE ONES
+// ?element.appendChild(childElement)
+
+
+//? insertBefore('element', 'location');
+
+// const result = document.querySelector('#result');
+// const first = document.querySelector('.red');
+// // create empty element
+// const bodyDiv = document.createElement('div');
+// // create textNode
+// const text = document.createTextNode('a simple body div');
+// bodyDiv.appendChild(text);
+// document.body.insertBefore(bodyDiv, result);
+
+
+// const heading = document.createElement('h2');
+// const headingText = document.createTextNode('dynamic heading');
+// heading.appendChild(headingText);
+// heading.classList.add('blue');
+// result.insertBefore(heading, first)
+
+
+// console.log(result.children);
+
+// ? replaceChild('new', 'old');
+
+// const result = document.querySelector('#result');
+// const first = document.querySelector('.red');
+// // create empty element
+// const bodyDiv = document.createElement('div');
+// // create textNode
+// const text = document.createTextNode('a simple body div');
+// bodyDiv.appendChild(text);
+// document.body.insertBefore(bodyDiv, result);
+
+
+// const heading = document.createElement('h2');
+// const headingText = document.createTextNode('dynamic heading');
+// heading.appendChild(headingText);
+// heading.classList.add('blue');
+// result.insertBefore(heading, first)
+
+// const smallHeading = document.createElement('h6');
+// const smallText = document.createTextNode(`I am small heading text`);
+// smallHeading.classList.add('red');
+// smallHeading.appendChild(smallText);
+// document.body.replaceChild(smallHeading,bodyDiv);
+
+// console.log(result.children);
+
+// !prepend
+// !innerText
+
+// const heading = document.createElement('h2');
+// heading.innerText = `i am a dynamic heading`;
+// document.body.prepend(heading);
+
+
+
+// !remove
+// removeChild
+
+// const result = document.querySelector('#result');
+// // result.remove();
+// const heading = result.querySelector('h1');
+// result.removeChild(heading);
+
+
+//! innerHTML
+// textContent
+
+// const list = document.getElementById('first');
+// const div = document.getElementById('second');
+// const item = document.querySelector('.item');
+// console.log(div.textContent);
+// console.log(list.innerHTML);
+
+// const ul = document.createElement('ul');
+// ul.innerHTML = `  <li class="item">list item</li>
+// <li>list item</li>`;
+
+// document.body.appendChild(ul);
+
+
+
+// !CSS
+
+// const random = document.querySelector('.random');
+// // random.style.backgroundColor = 'blue';
+// // random.style.color = 'white';
+// // random.style.fontSize = '3rem';
+// // random.style.textTransform = 'capitalize';
+
+// random.classList.add('title');
+
+
+
+// !events overview
+// select element
+// addEventListener()
+// what event, what to do
+
+
+// const btn = document.querySelector('.btn');
+// const heading = document.querySelector('h2');
+
+// btn.addEventListener('click', function(){
+//     heading.classList.add('red');
+// });
+
+
+// !function reference
+
+
+// const btn = document.querySelector('.btn');
+// const heading = document.querySelector('h2');
+
+// function changeColors(){
+   
+//     let hasClass = heading.classList.contains('red');
+
+//     if (hasClass) {
+//         heading.classList.remove('red');
+//     } else {
+//         heading.classList.add('red');
+//     }
+
+// }
+// btn.addEventListener('click', changeColors);
+
+
+
+//!Mouse Events
+
